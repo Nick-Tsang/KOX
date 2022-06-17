548c1124934faa347c391c2cd627936a586b6af7
Nick-Tsang <53812613+Nick-Tsang@users.noreply.github.com>
2022-06-17 10:20:19 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "f5833406-ede3-11ec-8ea0-0242ac120002",
                         "alterId": 0
                     }
                 ],
